cf13b1336608d60971043e20cacebe5d5ba9ebc1
Caroline Zhao <czhao67@jh.edu>
2024-10-21 10:53:48 -0400

    using Github Actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/myCovidDashboardApp/",  // TODO: Replace REPO_NAME with the name of your repository
   plugins: [react()],
-})
+});
